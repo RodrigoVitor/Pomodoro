61eb9bcf97bacf1b1f4c081c9b7a1d0aca1c0654
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2022-02-05 17:24:29 -0300

    app pomodoro

---
+++ src/js/script.js
@@ -0,0 +1,133 @@
+let minutes = document.querySelector('.minute')
+let second = document.querySelector('.second')
+let m = "00"
+let s = "00"
+let tmp;
+
+function start(value) {
+    if(value === "pomodoro") {
+        tmp = setInterval(() => {
+            //start second
+            s++
+            if( s < 10) {
+                s = '0' + s
+            }
+            //start minute
+            if(s > 60) {
+                s = '00'
+                m--
+                if(m < 10) {
+                    m = '0' + m
+                }
+            }
+            //pause timer
+            if(m == 00) {
+                clearInterval(tmp)
+            }
+
+            second.innerHTML = s
+            minutes.innerHTML = m
+        }, 1)
+    }
+
+    if(value === "short") {
+        tmp = setInterval(() => {
+            //start second
+            s++
+            if( s < 10) {
+                s = '0' + s
+            }
+            //start minute
+            if(s > 60) {
+                s = '00'
+                m--
+                if(m < 10) {
+                    m = '0' + m
+                }
+            }
+            //pause timer
+            if(m == 00) {
+                clearInterval(tmp)
+            }
+
+            second.innerHTML = s
+            minutes.innerHTML = m
+        }, 1)
+    } 
+
+    if(value === "long") {
+        tmp = setInterval(() => {
+            //start second
+            s++
+            if( s < 10) {
+                s = '0' + s
+            }
+            //start minute
+            if(s > 60) {
+                s = '00'
+                m--
+                if(m < 10) {
+                    m = '0' + m
+                }
+            }
+            //pause timer
+            if(m == 00) {
+                clearInterval(tmp)
+            }
+
+            second.innerHTML = s
+            minutes.innerHTML = m
+        }, 1)}
+}
+
+function pause() {
+    clearInterval(tmp)
+}
+
+function reset(value) {
+    if (value === "pomodoro") {
+        clearInterval(tmp)
+        m = "25"
+        s = "00"
+        minutes.innerHTML = m
+        second.innerHTML = s
+    }
+
+    if (value === "short") {
+        clearInterval(tmp)
+        m = "05"
+        s = "00"
+        minutes.innerHTML = m
+        second.innerHTML = s
+    }
+
+    if (value === "long") {
+        clearInterval(tmp)
+        m = "10"
+        s = "00"
+        minutes.innerHTML = m
+        second.innerHTML = s
+    }
+}
+
+
+//pre load
+function onLoad(value) {
+    if (value === "pomodoro") {
+        m = "25"
+        minutes.innerHTML = m
+        second.innerHTML = s
+    }
+    if (value === "short") {
+        m = "05"
+        minutes.innerHTML = m
+        second.innerHTML = s
+    }
+
+    if (value === "long") {
+        m = "10"
+        minutes.innerHTML = m
+        second.innerHTML = s
+    }
+}
+
