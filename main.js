2f1c07db10be6dc336a09764c53d3b6702a1f252
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2022-02-05 17:53:52 -0300

    altering width

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const {app, BrowserWindow, autoUpdater} = require('electron')
 let mainWindow
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({})
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height:800
+    })
 
     mainWindow.loadURL(`file://${__dirname}/src/views/index.html`)
 })
